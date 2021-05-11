bb16268873d83432dafd84c963c8a7c9be228d1a
aDahirel <axel.dahirel@gmail.com>
2021-05-11 10:25:25 +0200

    Ajout de l'affichage de users lié à une session

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -4,28 +4,34 @@ namespace App\Controller;
 
 use App\Entity\Session;
 use App\Entity\User;
+use App\Repository\UserRepository;
+use App\Repository\SessionRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AdminController extends AbstractController
 {
+    public function __construct(UserRepository $userRepo, SessionRepository $sessionRepo)
+    {
+        $this->userRepo = $userRepo;
+        $this->sessionRepo = $sessionRepo;
+    }
+
     /**
      * @Route("/admin", name="admin")
      */
     public function index(): Response
     {
-        $userRepo = $this->getDoctrine()->getRepository(User::class);
-        $sessionRepo = $this->getDoctrine()->getRepository(Session::class);
 
-        $users = $userRepo->findBy([], null, 5);
+        $users = $this->userRepo->findBy([], null, 5);
 
-        $sessions = $sessionRepo->findBy([], null, 5);
+        $sessions = $this->sessionRepo->findBy([], null, 5);
 
         return $this->render('admin/index.html.twig', [
             'controller_name' => 'AdminController',
             'users' => $users,
-            'sessions' =>$sessions
+            'sessions' => $sessions
         ]);
     }
 
@@ -35,8 +41,13 @@ class AdminController extends AbstractController
     public function session($id): Response
     {
 
+        $session = $this->sessionRepo->findOneBy(['id' => $id]);
+
+        $users = $session->getUser();
+
         return $this->render('admin/session.html.twig', [
-            'id' => $id
+            'id' => $id,
+            'users' => $users
         ]);
     }
 }
