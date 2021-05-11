cfc16c8375a6e8675c66ecbdaeac9d0c04c413d4
Graiiig <gregory.verguin@hotmail.fr>
2021-05-11 14:54:51 +0200

    Ajout d'une carte dans "mon-compte" quand l'utilisateur n'a pas de session

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Repository\SessionRepository;
+use App\Service\SessionService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,14 +13,16 @@ class UserController extends AbstractController
     /**
      * @Route("/mon-compte", name="user_account")
      */
-    public function index(): Response
+    public function index(SessionService $sessionService, SessionRepository $sessionRepository): Response
     {
         if ($this->getUser()){
 
             $user = $this->getUser();
             $sessions = $user->getSessions();
+
+            $nextSession = $sessionService->getNextSessionInfo($sessionRepository);
             
-            return $this->render('user/index.html.twig', compact('sessions'));
+            return $this->render('user/index.html.twig', compact('sessions', 'nextSession'));
         }
         else {
             return $this->redirectToRoute('app_login');
