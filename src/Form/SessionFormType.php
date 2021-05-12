8bd4a4e083f2a6fbaacfa28ea21ae7bcb9f0fc9b
aDahirel <axel.dahirel@gmail.com>
2021-05-12 11:46:03 +0200

    Modification d'une session + FormType

--- src/Form/SessionFormType.php
+++ src/Form/SessionFormType.php
@@ -7,7 +7,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class SessionType extends AbstractType
+class SessionFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
