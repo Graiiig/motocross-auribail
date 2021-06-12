6532354854e4b74b33326f17e17219100ff29abc
Grégory VERGUIN <gregory.verguin@hotmail.fr>
2021-06-12 16:26:11 +0200

    commit avant le merge catastrophique de feature pdf

--- src/Repository/PendingListRepository.php
+++ src/Repository/PendingListRepository.php
@@ -35,7 +35,26 @@ class PendingListRepository extends ServiceEntityRepository
             ->setParameter('role', $role)
             ->setParameter('sessionId', $sessionId)
             ->setParameter('age', $age)
-            ->setMaxResults(75)
+            ->getQuery()
+            ->getResult();
+    }
+    //fonction pour retrouver les membres en fonction de leur age, roles et session
+    public function findMembersWithLicense($sessionId, $role, $operator, $age)
+    {
+        // On crée une requête dql
+        $qb = $this->getEntityManager()->createQueryBuilder();
+
+        return
+            $qb->select('pl')
+            ->from('App\Entity\PendingList', 'pl')
+            ->innerJoin('App\Entity\User', 'u', 'WITH', 'u.id = pl.user')
+            ->andWhere('u.roles LIKE :role')
+            ->andWhere('pl.session = :sessionId')
+            ->andWhere('TIMESTAMPDIFF(YEAR,u.birthdate,CURRENT_DATE()) ' . $operator . ' :age')
+            ->andWhere('u.license IS NOT NULL')
+            ->setParameter('role', $role)
+            ->setParameter('sessionId', $sessionId)
+            ->setParameter('age', $age)
             ->getQuery()
             ->getResult();
     }
@@ -75,4 +94,42 @@ class PendingListRepository extends ServiceEntityRepository
         //On crée la liste d'attente finale
         return array('adults' => $adultsPendingList, 'kids' => $kidsPendingList);
     }
+
+
+        // Fonction qui récupère une PL pour une session donnée
+        public function getPendingListOfLicensed($sessionId)
+        {
+            // *** Adultes *** //
+            //recuperation des membres admins dans la file d'attente
+            $adminsMembersInPending = $this->findMembersWithLicense($sessionId, '["ROLE_ADMIN"]', '>', 0);
+    
+            //recuperation des membres adultes dans la file d'attente
+            $adultsMembersInPending = $this->findMembersWithLicense($sessionId, '["ROLE_MEMBER"]', '>', 16);
+    
+            //recuperation des non membres adultes dans la file d'attente
+            $adultsNonMembersInPending = $this->findMembersWithLicense($sessionId, '["ROLE_NON_MEMBER"]', '>', 16);
+    
+            // Concaténation des trois tableaux
+            $adultsPendingList = array_merge($adminsMembersInPending, $adultsMembersInPending, $adultsNonMembersInPending);
+    
+            //On limite la liste d'attente à 75 personnes
+            $adultsPendingList = array_slice($adultsPendingList, 0, 75);
+    
+            // *** Kids *** //
+            //recuperation des membres enfants dans la file d'attente
+            $kidsMembersInPending = $this->findMembersWithLicense($sessionId, '["ROLE_MEMBER"]', '<=', 16);
+    
+            //recuperation des non membres enfants dans la file d'attente
+            $kidsNonMembersInPending = $this->findMembersWithLicense($sessionId, '["ROLE_NON_MEMBER"]', '<=', 16);
+    
+            // Concaténation des deux tableaux
+            $kidsPendingList = array_merge($kidsMembersInPending, $kidsNonMembersInPending);
+    
+            //On limite la liste d'attente à 15 personnes
+            $kidsPendingList = array_slice($kidsPendingList, 0, 15);
+    
+            //On crée la liste d'attente finale
+            return array('adults' => $adultsPendingList, 'kids' => $kidsPendingList);
+        }
+
 }
