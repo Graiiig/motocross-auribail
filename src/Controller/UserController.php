d460b0df25156db8aefde0e60490c0ebc6652bbc
Grégory VERGUIN <gregory.verguin@hotmail.fr>
2021-06-13 15:57:25 +0200

    upload d'avatar OK

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -7,16 +7,18 @@ use App\Repository\PendingListRepository;
 use App\Repository\SessionRepository;
 use App\Service\SessionService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\String\Slugger\SluggerInterface;
 
 class UserController extends AbstractController
 {
     /**
      * @Route("/mon-compte", name="user_account")
      */
-    public function index(SessionService $sessionService, SessionRepository $sessionRepository, Request $request, PendingListRepository $pendingListRepository): Response
+    public function index(SessionService $sessionService, SessionRepository $sessionRepository, Request $request, PendingListRepository $pendingListRepository, SluggerInterface $slugger): Response
     {
         // On vérifie que l'utilisateur est connecté
         if ($this->getUser()){
@@ -33,6 +35,33 @@ class UserController extends AbstractController
             if ($form->isSubmitted() && $form->isValid()) {
             // On récupère les informations
             $user = $form->getData();
+
+                        /** @var UploadedFile $profilePicture */
+                        $profilePictureFile = $form->get('profilePicture')->getData();
+
+                        // this condition is needed because the 'profilePicture' field is not required
+                        // so the PDF file must be processed only when a file is uploaded
+                        if ($profilePictureFile) {
+                            $originalFilename = pathinfo($profilePictureFile->getClientOriginalName(), PATHINFO_FILENAME);
+                            // this is needed to safely include the file name as part of the URL
+                            $safeFilename = $slugger->slug($originalFilename);
+                            $newFilename = $safeFilename.'-'.uniqid().'.'.$profilePictureFile->guessExtension();
+            
+                            // Move the file to the directory where profilePictures are stored
+                            try {
+                                $profilePictureFile->move(
+                                    $this->getParameter('profilePictures_directory'),
+                                    $newFilename
+                                );
+                            } catch (FileException $e) {
+                                // ... handle exception if something happens during file upload
+                            }
+            
+                            // updates the 'brochureFilename' property to store the PDF file name
+                            // instead of its contents
+                            $user->setProfilePicture($newFilename);
+                        }
+
             // On envoie les nouvelles informations en base de donnée
             $entityManager = $this->getDoctrine()->getManager();
             $entityManager->persist($user);
--- src/Form/UserFormType.php
+++ src/Form/UserFormType.php
@@ -5,13 +5,23 @@ namespace App\Form;
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\File;
+use Symfony\Component\Security\Core\Security;
 
 class UserFormType extends AbstractType
 {
+    protected $security;
+
+    public function __construct(Security $security)
+    {
+        $this->security = $security;
+    }
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
+       
         $builder
             ->add('email', null, [
                 'label'   => 'Addresse email',
@@ -57,12 +67,28 @@ class UserFormType extends AbstractType
                     'placeholder'=>'Date de naissance'
                 ]
             ])
+            
             // TODO : Chemin vers un avatar de base
-            ->add('profilePicture', null, [
-                'label'   => 'Téléchargez une image de profil', 
-                'attr'=> [
-                    'placeholder'=>'Téléchargez une image de profil'
-                ]
+            ->add('profilePicture', FileType::class, [
+                'label' => 'Importez un avatar',
+                'data_class' => null,
+                'attr' => ['placeholder' => $this->security->getUser()->getProfilePicture()],
+                // make it optional so you don't have to re-upload the PDF file
+                // every time you edit the Product details
+                'required' => false,
+
+                // unmapped fields can't define their validation using annotations
+                // in the associated entity, so you can use the PHP constraint classes
+                'constraints' => [
+                    new File([
+                        'maxSize' => '1024k',
+                        'mimeTypes' => [
+                            'image/jpeg',
+                            'image/png',
+                        ],
+                        'mimeTypesMessage' => 'Merci d\'uploader une image au format JPG/PNG valide',
+                    ])
+                ],
             ])
         ;
     }
